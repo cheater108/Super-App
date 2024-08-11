ffdd031d981da29178aaf069f99d1b6d059370c9
Chetan Anand <chetan108@outlook.com>
2024-08-11 17:16:46 +0530

    added movies button

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,36 +3,47 @@ import styles from "./Dashboard.module.css";
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import News from "../components/News";
-
+import { useNavigate } from "react-router-dom";
 import Note from "../components/Note";
 import Timer from "../components/Timer";
 import Weather from "../components/Weather";
 
 function Dashboard() {
+    const navigate = useNavigate();
     const { user, selectedGenre } = useContext(AppContext);
     return (
-        <div className={styles.container}>
-            <div className={styles.div1}>
-                <Account
-                    name={user?.name}
-                    username={user?.username}
-                    email={user?.email}
-                    genres={selectedGenre}
-                />
+        <>
+            <div className={styles.container}>
+                <div className={styles.div1}>
+                    <Account
+                        name={user?.name}
+                        username={user?.username}
+                        email={user?.email}
+                        genres={selectedGenre}
+                    />
+                </div>
+                <div className={styles.div2}>
+                    <Weather />
+                </div>
+                <div className={styles.div3}>
+                    <Timer />
+                </div>
+                <div className={styles.div4}>
+                    <Note />
+                </div>
+                <div className={styles.div5}>
+                    <News />
+                </div>
             </div>
-            <div className={styles.div2}>
-                <Weather />
+            <div>
+                <button
+                    className={styles.btn}
+                    onClick={() => navigate("/movies")}
+                >
+                    Browse
+                </button>
             </div>
-            <div className={styles.div3}>
-                <Timer />
-            </div>
-            <div className={styles.div4}>
-                <Note />
-            </div>
-            <div className={styles.div5}>
-                <News />
-            </div>
-        </div>
+        </>
     );
 }
 
